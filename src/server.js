11a83e4e91d56154b3e0c087f3aa42da26b4248d
WowCole <tda.cole@gmail.com>
2022-10-31 14:27:45 +0900

    learning well

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
 const PORT = 4000;
-
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = () => console.log("Sombody is trying to go home.");
+app.use(logger);
 
-app.get("/", handleHome);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
