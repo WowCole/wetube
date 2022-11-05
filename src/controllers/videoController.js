2d98e074e08584e81f9677faae3b8f6b69a774dc
WowCole <tda.cole@gmail.com>
2022-11-05 11:37:41 +0900

    learing alor

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,1 +1,51 @@
-const trending = (req, res) => res.send("Home Page Videos");
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minuites ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minuites ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minuites ago",
+    views: 3,
+    id: 1,
+  },
+  {
+    title: "Fourth Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minuites ago",
+    views: 59,
+    id: 4,
+  },
+];
+
+export const trending = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  console.log("Show video", id);
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit Video" });
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+  res.send("Delete Video");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-const handleWatch = (req, res) => res.send("Watch Video");
-const handleEdit = (req, res) => res.send("Edit Video");
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
-videoRouter.get("/watch", handleWatch);
-videoRouter.get("/edit", handleEdit);
 export default videoRouter;
